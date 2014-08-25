03b83b6ae044b47502f45c55950a9811e1bb859e
Magion <vl.obdrzalek@gmail.com>
2014-08-25 20:31:30 +0200

    Update CrewCheck.cs

--- Source/CrewCheck.cs
+++ Source/CrewCheck.cs
@@ -12,6 +12,7 @@ namespace WernherChecker
         static bool launchBlocked;
         static GUIStyle buttonStyle = new GUIStyle(HighLogic.Skin.button);
         static bool isManned = false;
+        static bool launchAllowed = true;
         
 
         public static void CheckLaunchButton(Vector2 mousePos)
@@ -21,13 +22,20 @@ namespace WernherChecker
                 isManned = false;
                 foreach (Part part in EditorLogic.SortedShipList)
                 {
-                    if (part.CrewCapacity > 0) isManned = true;
+                    if (part.CrewCapacity > 0)
+                        isManned = true;
                 }
             }
 
-            if (launchBlock.Contains(mousePos) && EditorLogic.startPod != null && isManned && !
+            foreach (string lockKey in InputLockManager.lockStack.Keys)
+            {
+                if (InputLockManager.GetControlLock(lockKey) == ControlTypes.EDITOR_LAUNCH && lockKey != "WernherChecker_launchBlock")
+                    launchAllowed = false;
+                else
+                    launchAllowed = true;
+            }
 
-InputLockManager.lockStack.ContainsKey("EditorLogic_launchSequence"))
+            if (launchBlock.Contains(mousePos) && EditorLogic.startPod != null && isManned && !InputLockManager.lockStack.ContainsKey("EditorLogic_launchSequence") && launchAllowed)
             {
                 if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
                 {
@@ -55,15 +63,13 @@ InputLockManager.lockStack.ContainsKey("EditorLogic_launchSequence"))
 
         static void DrawWindow()
         {
-            crewWindow = GUILayout.Window(54, crewWindow, Window, "Have you checked the crew assignment?", 
-
-GUILayout.Width(300f));
+            crewWindow = GUILayout.Window(54, crewWindow, Window, "Have you checked the crew assignment?", GUILayout.Width(300f));
             crewWindow.center = new Vector2(Screen.width / 2, Screen.height / 2);
         }
 
         static void Window(int WindowID)
         {
-            if (GUILayout.Button("Yes, I have.", buttonStyle))
+            if (GUILayout.Button("Yes, I have. Go for LAUNCH!", buttonStyle))
             {
                 RenderingManager.RemoveFromPostDrawQueue(54, DrawWindow);
                 crewWindowDisplayed = false;
