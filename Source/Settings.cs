7f4e0731691291ce43fb6b8a81d5821a3bb70f02
Magion <vl.obdrzalek@gmail.com>
2014-08-25 20:32:29 +0200

    Update Settings.cs

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -8,17 +8,18 @@ namespace WernherChecker
 {
     class WCSettings
     {
-        public ConfigNode cfg = ConfigNode.Load(WernherChecker.DataPath + "WernherChecker.cfg");
+        public ConfigNode cfg;
         public bool j_sugg = true;
         public bool lockOnHover = true;
         public bool checkCrewAssignment = true;
         public bool cfgFound;
-        public WernherChecker.toolbarType activeToolbar;
+        public WernherChecker.toolbarType wantedToolbar;
 
         public void Load()
         {
             if (System.IO.File.Exists(WernherChecker.DataPath + "WernherChecker.cfg"))
             {
+                cfg = ConfigNode.Load(WernherChecker.DataPath + "WernherChecker.cfg");
                 cfgFound = true;
                 Debug.Log("[WernherChecker]: Config file found at " + WernherChecker.DataPath + "WernherChecker.cfg");
                 //-----------------------------------------------------------------------------
@@ -32,7 +33,7 @@ namespace WernherChecker
                 try
                 {
                     this.lockOnHover = bool.Parse(cfg.GetValue("lockOnHover"));
-                    Debug.Log("[WernherChecker]: SETTINGS - Lock editor while hovering over window: " + this.lockOnHover);
+                    Debug.Log("[WernherChecker]: SETTINGS - Lock editor while hovering over the main window: " + this.lockOnHover);
                 }
                 catch { Debug.LogWarning("[WernherChecker]: SETTINGS - lockOnHover field has an invalid value assigned (" + cfg.GetValue("lockOnHover") + "). Please assign valid boolean value."); }
                 //----------------------------------------------------------------------------
@@ -45,8 +46,8 @@ namespace WernherChecker
                 //--------------------------------------------------------------------------
                 try
                 {
-                    this.activeToolbar = (WernherChecker.toolbarType)Enum.Parse(typeof(WernherChecker.toolbarType), cfg.GetValue("toolbarType"));
-                    Debug.Log("[WernherChecker]: SETTINGS - Active toolbar: " + this.activeToolbar.ToString());
+                    this.wantedToolbar = (WernherChecker.toolbarType)Enum.Parse(typeof(WernherChecker.toolbarType), cfg.GetValue("toolbarType"));
+                    Debug.Log("[WernherChecker]: SETTINGS - Active toolbar: " + this.wantedToolbar.ToString());
                 }
                 catch { Debug.LogWarning("[WernherChecker]: SETTINGS - toolbarType field has an invalid value assigned (" + cfg.GetValue("toolbarType") + "). Please assign valid value (BLIZZY / STOCK)."); }
             }
